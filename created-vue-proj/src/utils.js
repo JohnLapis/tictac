6f91b93ec131f06635920e09820eeba19928c224
john <borges-2014@live.com>
2021-07-30 17:45:12 -0300

    style: Apply eslint

--- created-vue-proj/src/utils.js
+++ created-vue-proj/src/utils.js
@@ -1,17 +1,15 @@
-module.exports = ({requiredLineLength, gridDimension}) => {
-  const validDistances = [{x: 1, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}]
-
-  function getDistance(square1, square2) {
+module.exports = ({ requiredLineLength, gridDimension }) => {
+  function getDistance (square1, square2) {
     return {
       x: Math.abs(square1.pos.x - square2.pos.x),
-      y: Math.abs(square1.pos.y - square2.pos.y),
+      y: Math.abs(square1.pos.y - square2.pos.y)
     }
   }
-  function compareDistance(dist1, dist2) {
+  function compareDistance (dist1, dist2) {
     return dist1.x === dist2.x && dist1.y === dist2.y
   }
 
-  function squaresAreEquallyDistanced(squares, distance) {
+  function squaresAreEquallyDistanced (squares, distance) {
     if (squares.length < 2) return true
     // Squares are ordered from top to bottom and left to right
     squares = squares.sort(s => `${s.pos.y}${s.pos.x}`)
@@ -23,42 +21,42 @@ module.exports = ({requiredLineLength, gridDimension}) => {
     return true
   }
 
-  function getLine(squares, symbol) {
+  function getLine (squares, symbol) {
     squares = squares.filter(s => s.symbol === symbol)
 
     // Main diagonal
     let squaresInSameDiagonal = squares.filter(s => s.pos.y - s.pos.x === 0)
-    if(squaresInSameDiagonal.length === requiredLineLength
-       && squaresAreEquallyDistanced(squaresInSameDiagonal, {x: 1, y: 1})) {
-      return squaresInSameDiagonal;
+    if (squaresInSameDiagonal.length === requiredLineLength &&
+       squaresAreEquallyDistanced(squaresInSameDiagonal, { x: 1, y: 1 })) {
+      return squaresInSameDiagonal
     }
     // Reflected diagonal
     squaresInSameDiagonal = squares.filter(s => s.pos.y + s.pos.x === gridDimension - 1)
-    if(squaresInSameDiagonal.length === requiredLineLength
-         && squaresAreEquallyDistanced(squaresInSameDiagonal, {x: 1, y: 1})) {
-      return squaresInSameDiagonal;
+    if (squaresInSameDiagonal.length === requiredLineLength &&
+         squaresAreEquallyDistanced(squaresInSameDiagonal, { x: 1, y: 1 })) {
+      return squaresInSameDiagonal
     }
 
     // Rows
     for (let j = 0; j < gridDimension; j++) {
       const squaresInSameRow = squares.filter(s => s.pos.y === j)
-      if(squaresInSameRow.length === requiredLineLength
-         && squaresAreEquallyDistanced(squaresInSameRow, {x: 1, y: 0})) {
-        return squaresInSameRow;
+      if (squaresInSameRow.length === requiredLineLength &&
+         squaresAreEquallyDistanced(squaresInSameRow, { x: 1, y: 0 })) {
+        return squaresInSameRow
       }
     }
 
     // Columns
     for (let i = 0; i < gridDimension; i++) {
       const squaresInSameCol = squares.filter(s => s.pos.x === i)
-      if(squaresInSameCol.length === requiredLineLength
-         && squaresAreEquallyDistanced(squaresInSameCol, {x: 0, y: 1})) {
-        return squaresInSameCol;
+      if (squaresInSameCol.length === requiredLineLength &&
+         squaresAreEquallyDistanced(squaresInSameCol, { x: 0, y: 1 })) {
+        return squaresInSameCol
       }
     }
 
     return false
   }
 
-  return {getLine}
+  return { getLine }
 }
--- created-vue-proj/tests/unit/utils.spec.js
+++ created-vue-proj/tests/unit/utils.spec.js
@@ -1,99 +1,99 @@
-const setupUtils = require("../../src/utils.js")
+const setupUtils = require('../../src/utils.js')
 
-function makeSquare(x, y, symbol) {
-  return {pos: {x, y}, symbol}
+function makeSquare (x, y, symbol) {
+  return { pos: { x, y }, symbol }
 }
 
-function convertGridIntoArray(grid) {
-  let array = []
+function convertGridIntoArray (grid) {
+  const array = []
 
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid.length; j++) {
       array.push({
-        pos: {x: j, y: i},
-        symbol: grid[i][j],
+        pos: { x: j, y: i },
+        symbol: grid[i][j]
       })
     }
   }
   return array
 }
 
-describe("getLine with length 3", function() {
-  const {getLine} = setupUtils({requiredLineLength: 3, gridDimension: 3})
+describe('getLine with length 3', function () {
+  const { getLine } = setupUtils({ requiredLineLength: 3, gridDimension: 3 })
 
   it.each([
     [
-      [["X", "O", "O"],
-       ["O", "X", "O"],
-       ["O", "O", "X"]],
-      "X",
-      [makeSquare(0, 0, "X"), makeSquare(1, 1, "X"), makeSquare(2, 2, "X")],
+      [['X', 'O', 'O'],
+        ['O', 'X', 'O'],
+        ['O', 'O', 'X']],
+      'X',
+      [makeSquare(0, 0, 'X'), makeSquare(1, 1, 'X'), makeSquare(2, 2, 'X')]
     ],
     [
-      [["X", "O", "O"],
-       [" ", "O", "O"],
-       [" ", "O", "X"]],
-      "O",
-      [makeSquare(1, 0, "O"), makeSquare(1, 1, "O"), makeSquare(1, 2, "O")],
+      [['X', 'O', 'O'],
+        [' ', 'O', 'O'],
+        [' ', 'O', 'X']],
+      'O',
+      [makeSquare(1, 0, 'O'), makeSquare(1, 1, 'O'), makeSquare(1, 2, 'O')]
     ],
     [
-      [["X", "O", "O"],
-       ["O", "O", "O"],
-       ["O", "O", "X"]],
-      "X",
-      false,
+      [['X', 'O', 'O'],
+        ['O', 'O', 'O'],
+        ['O', 'O', 'X']],
+      'X',
+      false
     ],
     [
-      [["X", "O", "X"],
-       ["O", "X", "O"],
-       ["O", "X", "O"]],
-      "X",
-      false,
+      [['X', 'O', 'X'],
+        ['O', 'X', 'O'],
+        ['O', 'X', 'O']],
+      'X',
+      false
     ],
     [
-      [["X", "O", "X"],
-       ["O", "X", "O"],
-       ["O", "X", "O"]],
-      "O",
-      false,
-    ],
-  ])("getLine", function(grid, symbol, expected) {
+      [['X', 'O', 'X'],
+        ['O', 'X', 'O'],
+        ['O', 'X', 'O']],
+      'O',
+      false
+    ]
+  ])('getLine', function (grid, symbol, expected) {
     const squares = convertGridIntoArray(grid)
     expect(getLine(squares, symbol)).toEqual(expected)
   })
 })
 
-describe("getLine with length 4", function() {
-  const {getLine} = setupUtils({requiredLineLength: 4, gridDimension: 4})
+describe('getLine with length 4', function () {
+  const { getLine } = setupUtils({ requiredLineLength: 4, gridDimension: 4 })
 
   it.each([
     [
-      [["X", "O", "O", "O"],
-       [" ", "X", "O", "X"],
-       [" ", "O", "X", "X"],
-       ["O", "O", "O", "X"]],
-      "O",
+      [['X', 'O', 'O', 'O'],
+        [' ', 'X', 'O', 'X'],
+        [' ', 'O', 'X', 'X'],
+        ['O', 'O', 'O', 'X']],
+      'O',
       [
-        makeSquare(3, 0, "O"),
-        makeSquare(2, 1, "O"),
-        makeSquare(1, 2, "O"),
-        makeSquare(0, 3, "O"),
-      ],
+        makeSquare(3, 0, 'O'),
+        makeSquare(2, 1, 'O'),
+        makeSquare(1, 2, 'O'),
+        makeSquare(0, 3, 'O')
+      ]
     ],
     [
-      [["X", "O", "O", "O"],
-       [" ", "X", "O", "X"],
-       [" ", "O", "X", "X"],
-       ["O", "O", "O", "X"]],
-      "X",
+      [['X', 'O', 'O', 'O'],
+        [' ', 'X', 'O', 'X'],
+        [' ', 'O', 'X', 'X'],
+        ['O', 'O', 'O', 'X']],
+      'X',
       [
-        makeSquare(0, 0, "X"),
-        makeSquare(1, 1, "X"),
-        makeSquare(2, 2, "X"),
-        makeSquare(3, 3, "X"),
-      ],
-    ],
-  ])("getLine", function(grid, symbol, expected) {
+        makeSquare(0, 0, 'X'),
+        makeSquare(1, 1, 'X'),
+        makeSquare(2, 2, 'X'),
+        makeSquare(3, 3, 'X')
+      ]
+    ]
+  ])('getLine', function (grid, symbol, expected) {
     const squares = convertGridIntoArray(grid)
     expect(getLine(squares, symbol)).toEqual(expected)
   })
